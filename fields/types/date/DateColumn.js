bd161e33b5e39a927f46937d2f5dbf600e3533f3
yicone <yicone@gmail.com>
2017-12-01 22:18:06 +0800

    the format of date column should be the same as date field if there's one

--- fields/types/date/DateColumn.js
+++ fields/types/date/DateColumn.js
@@ -14,7 +14,8 @@ var DateColumn = React.createClass({
 		const value = this.props.data.fields[this.props.col.path];
 		if (!value) return null;
 
-		const format = (this.props.col.type === 'datetime') ? 'MMMM Do YYYY, h:mm:ss a' : 'MMMM Do YYYY';
+		// [formate time by xiaoTuiMao · Pull Request #3896 · keystonejs/keystone](https://github.com/keystonejs/keystone/pull/3896)
+		const format = this.props.col.field.formatString || ((this.props.col.type === 'datetime') ? 'MMMM Do YYYY, h:mm:ss a' : 'MMMM Do YYYY');
 		return moment(value).format(format);
 	},
 	render () {
